4eed429ed83f724bdffaed186b9300875caafbe8
Jesse Russell <jesserussell411@gmail.com>
2022-10-21 10:53:34 -0600

    added stack

--- src/main/scala/DataStore.scala
+++ src/main/scala/DataStore.scala
@@ -1,5 +1,5 @@
 import scala.collection.mutable
-import scala.collection.mutable.{HashMap, Stack}
+import scala.collection.mutable.{HashMap, ListBuffer, Stack}
 
 class DataStore {
   type Store = String | mutable.Stack[String] | mutable.HashMap[String, String]
@@ -27,11 +27,59 @@ class DataStore {
    */
   def get(key: String): Option[String] = {
     data.get(key) match {
-      case Some(value) => value match {
+      case Some(store) => store match {
         case basic: String => Some(basic)
         case _ => throw new WrongTypeException
       }
       case None => None
     }
   }
+
+  def lpush(key: String, elements: Iterable[String]): Unit = {
+    data.getOrElseUpdate(key, new Stack[String]) match {
+      case stack: Stack[String] => stack.pushAll(elements)
+      case _ => throw new WrongTypeException
+    }
+  }
+
+  def lpop(key: String): Option[String] = {
+    data.get(key) match {
+      case Some(store) =>
+        store match {
+          case stack: Stack[String] => if (stack.nonEmpty) Some(stack.pop()) else None
+          case _ => throw new WrongTypeException
+        }
+      case None => None
+    }
+  }
+
+  def lpop(key: String, count: Int): Option[List[String]] = {
+    data.get(key) match {
+      case Some(store) =>
+        store match {
+          case stack: mutable.Stack[String] =>
+            val result = new ListBuffer[String]
+            var i = 0
+            while (i < count && stack.nonEmpty) {
+              result.addOne(stack.pop())
+              i += 1
+            }
+
+            if (result.isEmpty) None else Some(result.toList)
+          case _ => throw new WrongTypeException
+        }
+      case None => None
+    }
+  }
+
+  def lrange(key: String, start: Int, stop: Int): Option[Iterable[String]] = {
+    data.get(key) match {
+      case Some(store) => store match {
+        case stack: Stack[String] => Some(stack.slice(start, stop + 1).toList)
+        case _ => throw new WrongTypeException
+      }
+      case None => None
+    }
+  }
+
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,29 +9,50 @@ object Main extends App {
     val args = command.split("[^\\S\\r\\n]").filter(s => s.nonEmpty)
 
     if (args.nonEmpty) {
-      val result: String =
+      val result: Option[String] | String =
         try {
           args(0).toLowerCase() match {
             case "get" =>
               if (args.length != 2) throw new WrongNumberOfArgumentsException
 
-              data.get(args(1)) match {
-                case Some(value) => value
-                case None => "(nil)"
-              }
+              data.get(args(1))
             case "set" =>
               if (args.length != 3) throw new WrongNumberOfArgumentsException
-              
+
               data.set(args(1), args(2))
               "OK"
+            case "lpush" =>
+              if (args.length < 3) throw new WrongNumberOfArgumentsException
+
+              data.lpush(args(1), args.slice(2, args.length))
+              (args.length - 2).toString
+            case "lpop" =>
+              args.length match {
+                case 2 => data.lpop(args(1))
+                case 3 => data.lpop(args(1), args(2).toInt) match {
+                  case Some(output) => output.zipWithIndex.map((s, i) => s"${i.+(1)} \"$s\"").mkString("\n")
+                  case None => None
+                }
+              }
+            case "lrange" =>
+              if (args.length != 4) throw new WrongNumberOfArgumentsException
+
+              data.lrange(args(1), args(2).toInt, args(3).toInt) match {
+                case Some(range) => range.zipWithIndex.map((s, i) => s"${i + 1}) \"$s\"").mkString("\n")
+                case None => None
+              }
+
             case _ => s"Unknown Command '${args(0)}' with args: ${args.slice(1, args.length).map(a => s"'$a'").mkString(" ")}"
           }
         } catch {
-          // string ripped straight from redis output
           case e: WrongTypeException => "(error) WRONGTYPE Operation against a key holding the wrong kind of value"
           case e: WrongNumberOfArgumentsException => s"Wrong number of arguments for command ${args(0)}"
         }
-      println(result)
+      println(result match {
+        case s: String => s
+        case Some(s) => s
+        case None => "(nil)"
+      })
     }
   }
 }
